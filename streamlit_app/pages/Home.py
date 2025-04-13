e619552d131463a85db90ef6b3c93d69dcb5085b
Justin Guthrie <121321748+justing0909@users.noreply.github.com>
2025-04-13 16:06:33 -0400

    updates to title

--- streamlit_app/pages/Home.py
+++ streamlit_app/pages/Home.py
@@ -1,18 +1,17 @@
 import streamlit as st
 
 def show():
-    st.title("Transit Delay Prediction")
+    st.title("Transit Duration Prediction App")
     st.markdown("""
-# Transit Delay Prediction App
 Justin Guthrie, Edward Liu
 DS4420 | Spring 2025
 
 ## The Methods:
 ### Baysian Modeling:
-We use Bayesian modeling to predict the delay of the MBTA subway system based on weather data. The model is trained on historical data and incorporates uncertainty in the predictions.
+We use Bayesian modeling to predict the duration of the MBTA subway system between Ruggles and Downtown Crossing based on weather data. The model is trained on historical data and incorporates uncertainty in the predictions.
 
 ### MLP
-We use a Multi-Layer Perceptron (MLP) to predict the delay of the MBTA subway system based on weather data. The MLP is trained on historical data and uses a feedforward architecture with multiple layers. We specifically index into the stations of Ruggles and Downtown Crossing as it is a common route for students looking to get into the city.
+We also use a Multi-Layer Perceptron (MLP) to predict the duration of the MBTA subway system between Ruggles and Downtown Crossing based on weather data. The MLP is trained on historical data and uses a feedforward architecture with multiple layers. We specifically index into the stations of Ruggles and Downtown Crossing as it is a common route for students looking to get into the city, and this model is represented in the app.
 
 ### Preprocessing
 Preprocessing using MBTA data found here: https://mbta-massdot.opendata.arcgis.com/datasets/5f71a5c035fc4a4dad1b7fa73ba27ef8/about
