0380e640c932b6d7a992c20afb3f2199832c0058
liueddy <156145485+liueddy@users.noreply.github.com>
2025-04-14 00:09:15 -0400

    Add files via upload

--- streamlit_app/pages/Predictions.py
+++ streamlit_app/pages/Predictions.py
@@ -11,9 +11,8 @@ def show():
     temperature = st.number_input("Temperature (°C)", value=20.0)
     precipitation = st.number_input("Precipitation (mm)", value=0.0)
     wind_speed = st.number_input("Wind Speed (km/h)", value=10.0)
-    cloud_cover = st.number_input("Cloud Cover (%)", value=50.0)
+    humidity = st.number_input("Cloud Cover (%)", value=50.0)
     visibility = st.number_input("Visibility (m)", value=10000.0)
-    bias = st.number_input("Bias Column", value=1.0, disabled=True)
 
     # Predict button
     if st.button("Predict"):
@@ -21,9 +20,9 @@ def show():
             "temperature_2m (°C)": temperature,
             "precipitation (mm)": precipitation,
             "wind_speed_10m (km/h)": wind_speed,
-            "cloud_cover (%)": cloud_cover,
+            "relative_humidity_2m (%)": humidity,
             "visibility (m)": visibility,
-            "bias_column": bias
+            "bias": 1
         }
         prediction = predict_duration(input_data)
-        st.success(f"Predicted Travel Duration: {prediction / 60:.3f} minutes")
+        st.success(f"Predicted Travel Duration: {prediction:.2f} seconds")
